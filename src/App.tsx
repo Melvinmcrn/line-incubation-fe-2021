e58372bfadbb31371eac7415716c0f22e7c8d233
Melvin Macaranas <greatwinblowaway@gmail.com>
2021-02-20 11:37:46 +0700

    craete base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "antd";
+
+import SendMelodyPage from "./views/LINEMelody/SendMelodyPage";
+
+import "antd/dist/antd.css";
+
+const { Content, Header } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout style={{height: "100vh"}}>
+        <Header>Header</Header>
+        <Content>
+          <SendMelodyPage />
+        </Content>
+      </Layout>
     </div>
   );
 }
